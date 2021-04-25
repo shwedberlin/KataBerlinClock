83c130f8c9a4b8352c8bd9c0aee44d202c8ea85c
Aleksej Edin <Aleksej.Edin@adesso.de>
2021-04-25 12:54:52 +0200

    New VS Solution

---
+++ BerlinClock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BerlinClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
